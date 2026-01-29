bda6863a1fed8bf98ec761f5e9d0ee4b0879c43d
Sanketh <sankeths908@gmail.com>
2026-01-30 05:09:42 +0530

    Add files via upload

---
+++ java_vscode/find_sum_k.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class find_sum_k {
+   void find_sum(int k)
+    {
+       HashSet<Integer> h=new HashSet<>();
+       int[] a=new int[5] ={1,2,3,4,5,6,}; 
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the number :");
+        
+       
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+      find_sum_k i=new find_sum_k();
+      i.find_sum(n);
+    }
+}
